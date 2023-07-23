fc1e0f9b54651fed6c9aad1585afcf1850437c6d
Facundo Alaniz <alanizfacundo902@gmail.com>
2023-07-23 00:39:07 -0300

    .

--- blogdjango/settings.py
+++ blogdjango/settings.py
@@ -85,6 +85,7 @@ WSGI_APPLICATION = 'blogdjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+""" 
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
@@ -92,6 +93,18 @@ DATABASES = {
         conn_max_age=600
     )
 }
+
+"""
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 
 # Password validation
@@ -129,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
